efd09b1fb0f186b54b8e14133a614826856f7d39
Corentin Cadiou <corentin.cadiou@cphyc.me>
2023-08-30 15:08:34 +0200

    Also install header files

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,3 +8,8 @@ cd build
 cmake ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} ../
 make -j${CPU_COUNT}
 make install
+
+# Copy header files
+mkdir -p ${PREFIX}/include/rockstar-galaxies/io
+install -m 644 ../*.h ${PREFIX}/include/rockstar-galaxies
+install -m 644 ../io/*.h ${PREFIX}/include/rockstar-galaxies/io
