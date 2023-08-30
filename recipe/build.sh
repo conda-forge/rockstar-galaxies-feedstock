105b10709844bb444d63f7d5b7c631a6778f9762
Corentin Cadiou <corentin.cadiou@cphyc.me>
2023-08-30 14:57:15 +0200

    Split in two packages with and without the lib

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,3 +8,8 @@ cd build
 cmake ${CMAKE_ARGS} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${PREFIX} ../
 make -j${CPU_COUNT}
 make install
+
+# Copy header files
+mkdir -p ${PREFIX}/include/rockstar-galaxies/io
+install ../*.h ${PREFIX}/include/rockstar-galaxies
+install ../io/*.h ${PREFIX}/include/rockstar-galaxies/io
